7aa06f20c5cd73c6f938d1fe4ea98a3040572105
Hilenn <hilenna@seznam.cz>
2020-11-16 15:19:49 +0100

    seeder added   and migrations create excluding Message table

---
+++ database/migrations/2020_11_16_135839_create_user_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('uploadedm_photo_path')->nullable();
+            $table->foreignId('locations_id')->nullable();//dont get in form but add to post.
+            $table->foreignId('service_categories_id')->nullable() ;
+            $table->boolean('volunteer')->default(0);//question
+            $table->string('cost' , 20 )->default('free');
+            $table->boolean('status_of_post')->default('1');
+            $table->text('description', 300);
+            $table->foreignId('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_posts');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(Handyhands::class);
         // \App\Models\User::factory(10)->create();
     }
 }
