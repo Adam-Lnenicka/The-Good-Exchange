9fbe959598790b3a54c6f673c590c195fd31c207
Hilenn <hilenna@seznam.cz>
2020-11-17 11:28:40 +0100

    working on drag and drop

--- resources/js/reactApp/Components/Location.jsx
+++ resources/js/reactApp/Components/Location.jsx
@@ -1,9 +1,16 @@
 import React, {useState, useEffect} from 'react';
 
 export default function Location(){
+   
+
+  const GrabLocation = () =>{
+    
+  }
+
+
     return (
   <>
-
+  I will find you 
   </>
 
     );
