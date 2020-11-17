549e7904a01184e9b9de98b85e1b78226afd5995
Hilenn <hilenna@seznam.cz>
2020-11-18 00:30:48 +0100

    croppa installed but not showing image

---
+++ app/Http/Livewire/TGELogo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+// view('livewire.t-g-e-logo');
+class TGELogo extends Component
+{
+    public function render()
+    {
+        return view('livewire.t-g-e-logo');
+    }
+}
