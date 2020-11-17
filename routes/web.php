3d9b8799867520a9caf83219755479d449028d79
Hilenn <hilenna@seznam.cz>
2020-11-17 15:14:43 +0100

    web route updated

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,6 @@ Route::put('/users/{id}', 'UserController@update');
 Route::get('/users/{id}/delete', 'UserController@delete'); // will show message "really want to delete?
 Route::delete('/users/{id}', 'UserController@destroy');
 
-Route::post('/register', 'Auth\RegisterController@store')->middleware(['guest']);
-
 
 Route::get('/message', 'MessageController@index');
 Route::post('/message', 'MessageController@store');
