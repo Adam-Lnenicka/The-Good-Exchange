4b49356de0a60c001bfd2347a13ceafc3b776abf
Hilenn <hilenna@seznam.cz>
2020-11-20 11:41:34 +0100

    bg-indigo-300

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-teal-300 h-24 border-b border-gray-400">
+<nav x-data="{ open: false }" class="bg-indigo-300  h-24 border-b border-gray-400">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between   h-16">
