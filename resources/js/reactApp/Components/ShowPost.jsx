607e69ad6af70700eb8e62e4e134084f2c6c0db9
Hilenn <hilenna@seznam.cz>
2020-11-24 22:38:56 +0100

    maps added working as best as  they can

--- resources/js/reactApp/Components/ShowPost.jsx
+++ resources/js/reactApp/Components/ShowPost.jsx
@@ -1,10 +1,9 @@
 import React, {useState, useEffect} from 'react';
 
-<<<<<<< HEAD
+import SimpleMap from './SimpleMap.jsx';
 import '../../../css/app.css';
+import Marker from './Marker';
 
-=======
->>>>>>> 60d5c1432d51bbf6e70b176ee9d12350f5395f02
 
 export default function ShowPost(props){
   const [{loading, loaded, data}, setDataState] = useState({
@@ -24,7 +23,6 @@ const fetchPosts = async () => {
       });
     const response = await fetch(url);
     const data = await response.json();
-<<<<<<< HEAD
 
     setDataState({
       loading: false,
@@ -32,15 +30,6 @@ const fetchPosts = async () => {
       data: data
   });
 
-=======
-
-    setDataState({
-      loading: false,
-      loaded: true,
-      data: data
-  });
-
->>>>>>> 60d5c1432d51bbf6e70b176ee9d12350f5395f02
   
 
   } 
@@ -55,7 +44,6 @@ let content = '';
 
     if (loading) {
         content = (
-<<<<<<< HEAD
             <div >
               <div className="loading">
                 <div className="circle"></div>
@@ -64,14 +52,6 @@ let content = '';
                 <div className="circle"></div>
              </div>
 
-=======
-            <div className="message">
-                <div className="animate-spin">
-                <svg className="animate-spin h-1 w-1 mr-3 ..." viewBox="0 0 24 24">
-                </svg>
-                Loading
-                </div>
->>>>>>> 60d5c1432d51bbf6e70b176ee9d12350f5395f02
             </div>
         )
     } else if (loaded) {
@@ -84,14 +64,14 @@ let content = '';
                     <div className=" p-2.5" >{id.description}</div>
                      <span className="">Cost:{id.cost}</span>  
                      <br/>
-                     <div>maplocation</div>
-<<<<<<< HEAD
-                     <a className=" bg-purple-600 -m-2mt-8 h-10 ml-2 px-2 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-purple-600 focus:outline-none focus:text-black focus:bg-purple-400 transition duration-150 ease-in-out"
+                     <SimpleMap/>
+                     <Marker
+                      lat={id.lat}
+                      lng={id.long}
+                      text="My Marker"
+                    />
+                     <a className=" bg-purple-600 mt-2.5 -m-2mt-8 h-10 ml-2 px-2 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-purple-600 focus:outline-none focus:text-black focus:bg-purple-400 transition duration-150 ease-in-out"
                      href={"/post/" + id.id + "/message"}>Send a message</a> 
-=======
-                     <a className=" bg-purple-600 -m-2mt-8 h-10 ml-2 px-2 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-purple-600 focus:outline-none focus:text-black focus:bg-purple-400 transition duration-150 ease-in-out"href="/users/create
-                     ">Send a message</a> 
->>>>>>> 60d5c1432d51bbf6e70b176ee9d12350f5395f02
                     </div>
                   ))
 
@@ -100,7 +80,6 @@ let content = '';
             </>
         )
     }
-<<<<<<< HEAD
    return (
   <>
 <div className="py-12 ">
@@ -113,27 +92,5 @@ let content = '';
     </div>
 </div>  
   </>
-=======
-
-   return (
-  <>
-  <div class="py-12">
-    <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-           
-           <img src="{}" alt=""/>  
-
-           <div>location</div>
-
-            <div>{text}</div>
-
-            <a href="">Send a message</a>
-        </div>
-    </div>
-</div>
-
-  </>
-
->>>>>>> 60d5c1432d51bbf6e70b176ee9d12350f5395f02
     );
 }
